8bb081182d37e33b0051e970ccc1376e4c6fae00
eolweus <37080270+eolweus@users.noreply.github.com>
2019-03-21 14:07:08 +0100

    Endret fra O til Ø i databasespørringer

--- src/treningsdagbok/NyOvelsegruppe.java
+++ src/treningsdagbok/NyOvelsegruppe.java
@@ -12,13 +12,13 @@ public class NyOvelsegruppe extends DBConn {
 	  // Legger til en ny gruppe i OvelseGruppe hvis den ikke finnes fra før
     public void nyOvelseGruppe(String gruppeNavn) {
         try {    
-            String query = String.format("SELECT Muskelgruppe FROM OvelseGruppe WHERE Muskelgruppe = %s", gruppeNavn);
+            String query = String.format("SELECT Muskelgruppe FROM ØvelseGruppe WHERE Muskelgruppe = %s", gruppeNavn);
             Statement statement = conn.createStatement();
             if (statement.execute(query)) {
                 System.out.println(String.format("Muskelgruppen: %s, eksisterer allerede i databasen", gruppeNavn));
                 return;
             }
-            String update = String.format("INSERT INTO OvelseGruppe Muskelgruppe(%s)",  gruppeNavn);
+            String update = String.format("INSERT INTO ØvelseGruppe Muskelgruppe(%s)",  gruppeNavn);
             statement.executeUpdate(update);
             System.out.println(String.format("Gruppen %s ble lagt til i databasen", gruppeNavn));
         } 
@@ -30,7 +30,7 @@ public class NyOvelsegruppe extends DBConn {
     // Henter ut gruppeID basert paa gruppeNavn, og setter saa inn (gruppeID, ovelseNavn) i OvelseIGruppe
     public void leggTilOvelseIGruppe(String gruppeNavn, String ovelseNavn) {
          try {
-        	 String query = String.format("SELECT GruppeID FROM OvelseGruppe WHERE Muskelgruppe = %s", gruppeNavn);
+        	 String query = String.format("SELECT GruppeID FROM ØvelseGruppe WHERE Muskelgruppe = %s", gruppeNavn);
         	 Statement statement = conn.createStatement();
         	 if (!statement.execute(query)) {
         		// Kommer man hit saa eksisterer det ingen gruppe med det navnet. Vi oppretter en slik gruppe.
@@ -39,9 +39,9 @@ public class NyOvelsegruppe extends DBConn {
         	 }
         	 rs = statement.getResultSet();
     		 int gruppeID = rs.getInt(1);
-    		 String update = String.format("INSERT INTO OvelseIGruppe VALUES(%d, '%s');", gruppeID, ovelseNavn );
+    		 String update = String.format("INSERT INTO ØvelseIGruppe VALUES(%d, '%s');", gruppeID, ovelseNavn );
     		 statement.executeUpdate(update);
-    		 System.out.println(String.format("Ovelse: %s er satt inn i gruppe: %d (%s)", ovelseNavn, gruppeID, gruppeNavn));
+    		 System.out.println(String.format("Øvelse: %s er satt inn i gruppe: %d (%s)", ovelseNavn, gruppeID, gruppeNavn));
 		} 
         catch (SQLException e) {
         	System.out.println("SQLException " + e.getMessage());
@@ -52,17 +52,19 @@ public class NyOvelsegruppe extends DBConn {
     // gruppeNavn hentes ut for aa printes ut saa man faar output som kan vise om man satte inn i onsket gruppe
     public void leggTilOvelseIGruppe(int gruppeID, String ovelseNavn) {
     	try {
-    		String query = String.format("INSERT INTO Muskelgruppe FROM OvelseGruppe WHERE GruppeID = %d", gruppeID);
+    		String query = String.format("INSERT INTO Muskelgruppe FROM ØvelseGruppe WHERE GruppeID = %d", gruppeID);
        	 	Statement statement = conn.createStatement();
        	 	statement.execute(query);
        	 	rs  = statement.getResultSet();
        	 	String gruppeNavn = rs.getString(2);			// Henter ut gruppeNavn
-       	 	String update = String.format("INSERT INTO OvelseIGruppe VALUES(%d, '%s');", gruppeID, ovelseNavn );
+       	 	String update = String.format("INSERT INTO ØvelseIGruppe VALUES(%d, '%s');", gruppeID, ovelseNavn );
        	 	statement.executeUpdate(update);
-       	 	System.out.println(String.format("Ovelse: %s er satt inn i gruppe: %d (%s)", ovelseNavn, gruppeID, gruppeNavn));
+       	 	System.out.println(String.format("Øvelse: %s er satt inn i gruppe: %d (%s)", ovelseNavn, gruppeID, gruppeNavn));
     	}
     	catch (SQLException e) {
     		System.out.println("SQLException " + e.getMessage());
     	}
     }
+    
+
 }
